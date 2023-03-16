d9e73c1933b1bd816929f97294f1b8c565cd7415
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-03-16 16:04:15 +0530

    [sanjana] Add : Create JSON File

--- ObjectOrientdProgramming/Program.cs
+++ ObjectOrientdProgramming/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome To Object Oriented Program");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientdProgramming
+{
+    public class Program
+    {
+       static void Main(string[] args)
+        {
+            InventoryOperation inventoryOperation = new InventoryOperation();
+            inventoryOperation.ReadJsonFile(@"C:\Users\SOURABH\Desktop\Day 3\ObjectOrientedProgramming\ObjectOrientdProgramming\UC1-CreateJSON\Inventory.json");
+        }
+    }
+}
---
+++ ObjectOrientdProgramming/UC1-CreateJSON/InventoryData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ObjectOrientdProgramming;
+
+namespace ObjectOrientdProgramming
+{
+    public class InventoryData
+    {
+        public String Name { get; set; }
+        public double Weight { get; set; }
+        public double PricePerKg { get; set; }
+    }
+}
